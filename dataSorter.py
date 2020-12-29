553ea45ee368ecd84465ec80c00fbc1980bf320d
henri <drheinrich940@gmail.com>
2020-12-29 22:36:03 +0100

    :sparkles: adding preprocessing and sorting functions

--- dataSorter.py
+++ dataSorter.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import os
 from shutil import copyfile
+import tqdm
 
-df = pd.read_csv('dataset/train.csv')
-print(df.head())
-print(df.Class.unique())
-# Create class folder
-if not os.path.exists('dataset/sortedTrain'):
-    os.makedirs('dataset/sortedTrain')
-    for item in df.Class.unique():
-        os.makedirs('dataset/sortedTrain/' + item)
+def sortData(srcDir, srcCSV, destDir):
+    df = pd.read_csv(srcCSV)
+    print(df.head())
+    print(df.Class.unique())
+    # Create class folder
+    if not os.path.exists(destDir):
+        os.makedirs(destDir)
+        for item in df.Class.unique():
+            os.makedirs(destDir + '\\' + item)
 
-# Copy images to associated folder
-for index, row in df.iterrows():
-    copyfile('dataset/train/' + row['Image'], 'dataset/sortedTrain/' + row['Class'] + '/' + row['Image'])
+    # Copy images to associated folder
+    for index, row in tqdm.tqdm(df.iterrows()):
+        copyfile(srcDir + '\\' + row['Image'], destDir + '\\' + row['Class'] + '\\' + row['Image'])
 
+sortData('dataset\\trainReformated', 'dataset\\train.csv', 'dataset\\trainSorted')
--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,56 @@
 import PIL
 import cv2
 import glob
+import os
+import tqdm
 
 dataTestPath = 'dataset\\test'
 dataTrainPath = 'dataset\\train'
-def displayDataInfo():
-    for testPath in glob.glob(dataTestPath+'/*'):
-        print(testPath)
 
-displayDataInfo()
+dataTestPathReformated = 'dataset\\testReformated'
+dataTrainPathReformated = 'dataset\\trainReformated'
+def displayDataInfo():
+    xChanList = []
+    yChanList = []
+    for path in glob.glob(dataTestPath + '/*'):
+        im = cv2.imread(path)
+        y, x, z = im.shape
+        yChanList.append(y)
+        xChanList.append(x)
+
+    for x in xChanList:
+        if x != 80:
+            print(x)
+    print(max(yChanList))
+
+    for path in glob.glob(dataTrainPath+'/*'):
+        im = cv2.imread(path)
+        y, x, z = im.shape
+        yChanList.append(y)
+        xChanList.append(x)
+
+    for x in xChanList:
+        if x != 80:
+            print(x)
+    print(max(yChanList))
+
+def reformatDataToFixedSize():
+    # Create new dirs
+    os.mkdir(dataTrainPathReformated)
+    os.mkdir(dataTestPathReformated)
+
+    for path in tqdm.tqdm(glob.glob(dataTestPath + '/*')):
+        im = cv2.imread(path)
+        y, _, _ = im.shape
+        replicate = cv2.copyMakeBorder(im, 0, 300 - y, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+        cv2.imwrite(dataTestPathReformated + '\\' + path.split('\\')[-1], replicate)
+
+    for path in tqdm.tqdm(glob.glob(dataTrainPath + '/*')):
+        im = cv2.imread(path)
+        y, _, _ = im.shape
+        replicate = cv2.copyMakeBorder(im, 0, 300-y, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
+
+        cv2.imwrite(dataTrainPathReformated + '\\' + path.split('\\')[-1], replicate)
+
+reformatDataToFixedSize()
