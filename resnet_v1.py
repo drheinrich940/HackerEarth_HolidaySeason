04419bacd6d3e3b4d4a3db216a97b83604461eb2
henri <drheinrich940@gmail.com>
2021-01-04 23:00:57 +0100

    :sparkles: adding a resnet v1 implementation

---
+++ resnet_v1.py
@@ -0,0 +1,13 @@
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import AveragePooling2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import ZeroPadding2D
+from keras.layers.core import Activation
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+from keras.models import Model
+from keras.layers import add
+from keras.regularizers import l2
+from keras import backend as K
