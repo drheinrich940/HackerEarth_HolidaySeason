007d7eac5038522b9f3baefa7dbb2052405fea64
Henri <drheinrich940@gmail.com>
2021-01-15 03:25:13 +0100

    :sparkles: :construction: Implementing a new result display function with better unit scaling, and display of value of interest (max & last acc during train, min & las loss during train)

--- trainingFunctions.py
+++ trainingFunctions.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
 from trainingConstants import *
-from pandas import read_csv, DataFrame
+import numpy as np
 
 def training(model, epochs):
     AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -67,18 +67,56 @@ def plot_training_results(history, epochs, model, save=False):
 
     epochs_range = range(epochs)
 
-    plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
+    fig, ax = plt.subplots(1, 2, figsize=(10, 10))
 
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
+    fig.subplots_adjust(wspace=.35)
+
+    colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    ax[0].plot(epochs_range, acc, label='Training Accuracy')
+    ax[0].plot(epochs_range, val_acc, label='Validation Accuracy')
+    ax[0].set_title('Training and Validation Accuracy')
+    ax[0].set_xlabel('epoch')
+    ax[0].set_ylabel('accuracy')
+    ax[0].set_yticks(np.arange(0, 1.05, 0.05))
+    ax[0].legend(loc='lower right')
+
+    ax[0].annotate('Max :%0.2f' % max(val_acc), xy=(1, max(val_acc)), xytext=(2, 8),
+                   xycoords=('axes fraction', 'data'),
+                   textcoords='offset points',
+                   transform=ax[0].transAxes,
+                   bbox=dict(boxstyle="round", fc=colors[1], ec=colors[1]))
+
+    ax[0].annotate('Last :%0.2f' % val_acc[-1], xy=(1, val_acc[-1]), zorder=1, xytext=(2, -8),
+                   xycoords=('axes fraction', 'data'),
+                   textcoords='offset points',
+                   bbox=dict(boxstyle="round", fc=colors[0], ec=colors[0]))
+
+    ax[1].plot(epochs_range, loss, label='Training Loss')
+    ax[1].plot(epochs_range, val_loss, label='Validation Loss')
+    ax[1].set_title('Training and Validation Loss')
+    ax[1].set_xlabel('epoch')
+    ax[1].set_ylabel('loss')
+    ax[1].set_yticks(np.arange(0, 1.05, 0.05))
+    ax[1].legend(loc='upper right')
+
+    ax[1].annotate('Min :%0.2f' % min(val_loss), xy=(1, min(val_loss)), xytext=(2, -8),
+                   xycoords=('axes fraction', 'data'),
+                   textcoords='offset points',
+                   transform=ax[0].transAxes,
+                   bbox=dict(boxstyle="round", fc=colors[1], ec=colors[1]))
+
+    ax[1].annotate('Last :%0.2f' % val_loss[-1], xy=(1, val_loss[-1]), zorder=1, xytext=(2, 8),
+                   xycoords=('axes fraction', 'data'),
+                   textcoords='offset points',
+                   bbox=dict(boxstyle="round", fc=colors[1], ec=colors[1]))
+
+    '''for var in (f1y, f2y):
+        print(var)
+        ax[1].annotate('%0.2f' % max(var), xy=(1, max(np.array(var))), xytext=(2, 0), 
+                     xycoords=('axes fraction', 'data'), textcoords='offset points', transform=ax[1].transAxes)
+    '''
+    plt.show()
 
     if save:
         data_aug = ''
